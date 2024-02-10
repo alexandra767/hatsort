95df697ab9e00eb6fa5aad2982af97ea5df282da
Alexandra Titus <alexandratitus767@gmail.com>
2024-02-10 12:00:49 -0500

    hat example of classmethod

--- hat.py
+++ hat.py
@@ -3,7 +3,7 @@ import random
 class Hat:
     houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
     #Solves chicken and egg problem
-    #I can call this with instantuation class object
+    #I can call this with not doing instantuation class object
     @classmethod
     def sort(cls, name):
         print(name, "is in", random.choice(cls.houses))
